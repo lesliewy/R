1983fd11b371e55db1fa27e84e5d036ea761c314
lesliewy <leslie_wangyang@163.com>
2016-11-22 21:27:40 +0800

    change

---
+++ shiny/Stock/stocknotion/utils.R
@@ -0,0 +1,41 @@
+
+indexes <<- list("上证指数" = "1A0001",
+                 "深证成指" = "399001",
+                 "创业板指" = "399006")
+
+corrNotionData <- function(dfIndex, dfNotions, cormethod){
+  print("this is in corrNotionData()")
+  groupNotions <- groupNotionData(dfNotions)
+  # 注意参数的传递
+  cordata = lapply(groupNotions, calOneCor, dfIndex, cormethod)
+  # 将list转成data.frame
+  result = {}
+  for(i in cordata){
+    result <- rbind(result, i)
+  }
+  # 发现按t值排序和按相关系数排序结果一样
+  result <- result[order(result$cor, decreasing=TRUE), ]
+  return(result)
+}
+
+# 对数组框按照factor分组. 把TRADE_DATE的factor去掉，不需要
+groupNotionData <- function(dfNotions){
+  print(paste("this is in groupNotionData()"))
+  
+  dfNotions$TRADE_DATE <- as.character(dfNotions$TRADE_DATE)
+  groupNotions <- split(dfNotions, dfNotions$NOTION_NAME)
+  return(groupNotions)
+}
+
+calOneCor <- function(dfNotion, dfIndex, cormethod){
+  #print(paste("this is in calOneCor()", cormethod))
+  # 大盘和板块的日期必须一致
+  intersectdates <- intersect(dfNotion$TRADE_DATE, dfIndex$TRADE_DATE)
+  
+  dfnotion1 <- dfNotion[which(dfNotion$TRADE_DATE %in% intersectdates), ]
+  dfIndex1 <- dfIndex[which(dfIndex$TRADE_DATE %in% intersectdates),]
+  cordata <- cor.test(dfnotion1$CHANGE_PCT, dfIndex1$CHANGE_PCT,method=cormethod)
+  data <- data.frame(notionName=c(as.character(dfNotion$NOTION_NAME)[1]), p.value=round(c(cordata$p.value), 10), cor=round(c(cordata$estimate), 10), 
+                     t=round(c(cordata$statistic), 10), alternative=c(cordata$alternative), method=c(cordata$method))
+  return(data)
+}
